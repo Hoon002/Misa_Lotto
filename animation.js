b821214fcbecf2e1a45b876cd1216416a49993f7
Hoon002 <62237335+Hoon002@users.noreply.github.com>
2022-02-16 04:56:16 +0900

    Add files via upload

--- animation.js
+++ animation.js
@@ -4,7 +4,7 @@ var rapid_change = {
         return Math.floor(Math.random() * max);
       },
 
-    number_scaling : function(selector, speed ,target_num) {
+    number_scaling : function(selector, csc, speed ,target_num) {
 
         $({ val : 0}).animate({ val : target_num}, {
             duration: speed,
@@ -13,6 +13,7 @@ var rapid_change = {
                 var num = Math.floor(this.val).toString();
                 $(selector).text(num);
             },
+
             complete: function() {
                 var num = Math.floor(this.val).toString();
                 $(selector).text(num);
@@ -21,7 +22,9 @@ var rapid_change = {
     },
 
     color_change : function(selector) {
-            $(selector).attr('stroke', 'white');
+            $(selector).animate({
+                backgroundColor : "balck"
+            }, 4000)
     }
 }
 
@@ -31,7 +34,7 @@ function lotto_result (result) {
         var result = [];
     }
 
-    let n = Math.floor(Math.random() * 99);
+    let n = Math.floor(Math.random() * 59);
 
     if (result.length < 5 && result.indexOf(n) < 0) { //리스트에 다섯 개가 차거나, 랜덤으로 새로 추출된 값이 이미 있는게 아니면 추가
         result.push(n);
